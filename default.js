496b32ca93e7ec437f2cc43448c0e6fb663e2c92
isabellaadam <69841235+isabellaadam@users.noreply.github.com>
2021-10-11 11:34:20 +0800

    Update default.js

--- default.js
+++ default.js
@@ -1,27 +1,10 @@
-//default array
 const studentsData = [
 	{
 		id: 1,
-		NIM: 105,
-		fullName: 'eca',
-		gender: 'Male',
-		fakultas: 'FIK',
-		study: 'Sistem Informasi',
-	},
-	{
-		id: 2,
-		NIM: 103021810001,
-		fullName: 'Jack Reacher',
-		gender: 'Male',
-		fakultas: 'FEB',
-		study: 'Manajemen',
-	},
-	{
-		id: 3,
-		NIM: 105021810002,
-		fullName: 'Mery Heather',
+		NIM: 105011910107,
+		fullName: 'Adam, Isabella',
 		gender: 'Female',
 		fakultas: 'FIK',
-		study: 'Informatika',
+		study: 'Sistem Informasi',
 	}
 ]
